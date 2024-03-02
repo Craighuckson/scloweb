ac9ec7761d395e01e62408f8a53908e43342d33d
Craig <craig.huckson@gmail.com>
2024-03-02 02:20:53 -0500

    Delete unused resources and files

--- app.js
+++ app.js
@@ -1,5 +1,71 @@
 import DrawingForm from './drawingform.js';
 
+UNDOSTACK = [];
+//dbConnection = new sqlite3.Database('drawing.db');
+SAVABLECHANGES = false;
+USERMODE = DRAWMODE.selectTool
+
+function Point(x, y) {
+    this.x = x;
+    this.y = y;
+}
+
+function dot(point1, point2) {
+    try {
+        let result;
+        let num = (point1.x * point2.x) + (point1.y * point2.y);
+        result = num;
+    } catch (ex) {
+        alert(ex);
+    }
+    return result;
+}
+
+const DRAWMODE = {
+    selectTool: 0,
+    drawLine: 1,
+    drawRoad: 2,
+    drawCable: 3,
+    drawText: 4,
+    undo: 5,
+    drawRectangle: 6,
+    drawCircle: 7,
+    move: 8,
+    drawMeasurement: 9,
+    drawSymbol: 10,
+    drawArc: 11,
+    insertSymbol: 12,
+    insertText: 13,
+    Rotate: 14,
+    InsertStamp: 15,
+    Resize: 16,
+    Clear: 17,
+};
+
+const LINESTYLE = {
+    solid: 0,
+    dashed: 1,
+    dotted: 2,
+};
+
+const SHAPETYPE = {
+    arc : 0,
+    circle : 1,
+    line : 2,
+    rectangle : 3,
+    group : 4,
+    stamp : 5,
+    text : 6,
+    triangle : 7,
+}
+
+const TEXTMODE = {
+    horizontal: 0,
+    vertical: 1,
+}
+
+
+
 window.onload = function() {
     DrawingForm.init();
 }
---
+++ drawsurface.js
@@ -0,0 +1,67 @@
+class DrawingSurface {
+    constructor(canvas) {
+        this.Shapes = [];
+        this.TextIDs = [];
+        this.SelectedShapes = [];
+        this.GridOn = false;
+        this.drawingissaving = false;
+        this.canvas = canvas;
+    }
+
+    get Width() {
+        //return the html canvas width
+        return this.canvas.width;
+    }
+
+    get Height() {
+        return this.canvas.height;
+    }
+
+    Add(Shape) {
+        try {
+            let num = this.Shapes.length;
+            Shape.ShapeID = num;
+            this.Shapes[num] = Shape;
+            if (Shape.ShapeType === GlobalVariables.ShapeType.Text) {
+                this.AddText(Shape);
+            }
+        } catch (ex) {
+            alert(ex)
+        }
+    }
+
+    AddText(thisText) {
+        try {
+            let num = this.TextIDs.length;
+            this.TextIDs[num] = thisText.ShapeID;
+        } catch (ex) {
+           alert(ex)
+        }
+    }
+
+    RemoveText(thisText) {
+        try {
+            for (let i = 0; i < this.TextIDs.length; i++) {
+                if (this.TextIDs[i] === thisText.ShapeID) {
+                    this.TextIDs[i] = 0;
+                }
+            }
+        } catch (ex) {
+            alert(ex);
+        }
+    }
+
+    ChangeTextID(oldID, newID) {
+        try {
+            for (let i = 0; i < this.TextIDs.length; i++) {
+                if (this.TextIDs[i] === oldID) {
+                    this.TextIDs[i] = newID;
+                }
+            }
+        } catch (ex) {
+            alert(ex);
+        }
+    }
+}
+
+export default DrawingSurface;
